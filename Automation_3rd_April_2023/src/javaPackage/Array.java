f7f808bacaaced817dddb8c64a12ebd79701dcc9
A U Thor <author@example.com>
2023-06-29 00:21:02 +0530

    hi
    cc

---
+++ Automation_3rd_April_2023/src/javaPackage/Array.java
@@ -0,0 +1,17 @@
+package javaPackage;
+
+public class Array {
+	
+	public static void main(String[] args) {
+		 int y[] =new int[6];
+		int a[] = {1,2,3};
+		int j= a.length;
+		System.out.println(j);
+		
+		for (int i=0;i<j; i++) {
+			System.out.println(a[i]);
+			System.out.println("Hi");
+		}
+	}
+
+}
