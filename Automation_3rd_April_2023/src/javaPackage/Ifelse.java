6e30aa44bcc38dfe4f5da25a5734650bff73cccf
91952 <91952@LAPTOP-GV6PF2UQ>
2023-06-29 00:01:47 +0530

    commited first time

---
+++ Automation_3rd_April_2023/src/javaPackage/Ifelse.java
@@ -0,0 +1,16 @@
+package javaPackage;
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+		int x = 20;
+		int y = 20;
+		if (x == y) {
+			System.out.println("x is equal to y");
+		} else {
+			System.out.println("x is not equal to y...");
+		}
+
+	}
+
+}
